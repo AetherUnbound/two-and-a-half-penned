970822dce4c3e4d5c570ded6228dd01d4e0626d2
AetherUnbound <bowdenm@spu.edu>
2015-12-14 18:23:21 -0800

    Further cleaning
    Added signature and copyright cleaning as well as extra end-of-string
    newline cleaning

--- thp.py
+++ thp.py
@@ -31,6 +31,14 @@ def getUserToQuery():
     #Eventually this may be made mutable
     return "charliesheen"
 
+def strDel(toDel, delFrom):
+    if(toDel > -1):
+        print "Char found at: " + str(toDel)
+        #delFrom = delFrom[:toDel]
+        return True
+    else:
+        return False
+
 def stripTweet(tweet):
     #Charlie has a lot of extra crap in his tweets
     #(copyrights, 'x', twitter links, etc)
@@ -39,17 +47,33 @@ def stripTweet(tweet):
     #   and hashtags after the copyright symbol. Perhaps making
     #   a substring of everything up to the symbol will cut out a
     #   good deal. Then check after for his 'signature'.
-    #tweet = tweet.decode("utf8")
+
+    #Char cleaning
+    xIndex = tweet.find('\nx')
+    xoxIndex = tweet.find('xox')
     cIndex = tweet.find('\xa9')
-    print "cIndex: " + str(cIndex)
-    if(cIndex > -1):
+    #'x' cleaning
+    if(strDel(xIndex, tweet)):
+        tweet = tweet[:xIndex]
+    #'xox' cleaning
+    elif(strDel(xoxIndex,tweet)):
+        tweet = tweet[:xoxIndex]
+    # copyright cleaning
+    elif(strDel(cIndex, tweet)):
         tweet = tweet[:cIndex]
+    #end of string newline cleaning
+    newlineFlag = False
+    while(not newlineFlag):
+        if(tweet.endswith("\n")):
+            tweet = tweet[:-1]
+        else:
+            newlineFlag = True
     print "Double newline found at index: " + str(tweet.find('\n\n'))
     print tweet
 
 def status(statusList, degree = 1):
     length = len(statusList)
-    rand = 6 #randint(0,(length-1))
+    rand = randint(0,(length-1))
     tweet = statusList[rand].text
     print "Length: " + str(length)
     print "Random Number: " + str(rand)
@@ -63,3 +87,4 @@ timeline = api.GetUserTimeline(screen_name=user,count=10,include_rts=False)
 print [t.GetText() for t in timeline]
 #print timeline[0].text
 status(timeline)
+print "EOT"
