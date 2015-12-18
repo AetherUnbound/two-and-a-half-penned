7140b410da762cacac3ceeb466cdadaa5358fb87
AetherUnbound <bowdenm@spu.edu>
2015-12-18 00:12:58 -0800

    Random substring determination
    Implemented the function that will determine, based on the slider, a
    random substring

--- thp.py
+++ thp.py
@@ -32,18 +32,73 @@ def getUserToQuery():
     return "charliesheen"
 
 def status(statusList, degree = 1):
-    #Function definitions
-    def strCut(index):
+    #    Function definitions
+    #
+    #
+    #cut the tweet once valid index is found
+    def strDel(toDel):
         global tweet
-        tweet = tweet[:index]
-    def strDel(toDel, delFrom):
-        if(toDel > -1):
-            print "Char found at: " + str(toDel)
-            #delFrom = delFrom[:toDel]
-            return True
-        else:
-            return False
+        index = tweet.find(toDel)
+        if(index > -1):
+            print "Char " + repr(toDel) + " found at: " + str(index)
+            tweet = tweet[:index]
 
+    #newline cleaning
+    def cleanNewline():
+        global tweet
+        newlineFlag = False
+        while(not newlineFlag):
+            if(tweet.endswith("\n") or tweet.endswith(' ')):
+                tweet = tweet[:-1]
+            else:
+                newlineFlag = True
+
+    #determine substring from degree
+    #   degree = 1: full tweet is returned
+    #   degree = 2: random 'paragraph' is returned (may end up being full tweet)
+    #   degree = 3: random line from random paragraph is returned
+    def subTweet():
+        #internal function for randomly selecting paragraph/line
+        def randSelect(newline, numNewline):
+            #finds the indexes of each newline
+            global tweet
+            start = 0
+            breakList = [0] #append starting location
+            flag = True
+            while(flag):
+                temp = tweet.find(newline, start)
+                if (temp == -1):
+                    breakList.append(len(tweet)) #append ending location
+                    flag = False
+                else:
+                    breakList.append(temp + numNewline) #+2 to skip over newline
+                    start = temp+1
+                    print repr(newline) + " found at index: " + str(temp)
+            print breakList
+            count = len(breakList)
+            start = randint(0,count-2) #-1 for zero indexed list and -1 to avoid string end as starting point
+            print "Random selection: " + str(start + 1)
+            tweet = tweet[breakList[start]:breakList[start+1]] #should return the portion of the string requested
+            cleanNewline()
+
+        #subTweet executed code
+        global tweet
+        if(degree == 1): #full tweet selected
+            return
+        else: #if degree is 2+
+            randSelect('\n\n',2) #select random paragraph
+            if(degree == 2):
+                return
+            else: #if degree is 3+
+                randSelect('\n',1) #select random line
+                if(degree == 3):
+                    return
+                elif(degree == 4):
+                    randSelect(' ',1) #select random word
+                    return
+
+    #    Executed code
+    #
     #find random tweet
     length = len(statusList)
     rand = randint(0,(length-1))
@@ -52,6 +107,7 @@ def status(statusList, degree = 1):
     print "Length: " + str(length)
     print "Random Number: " + str(rand)
     print "\nStripped Tweet: "
+
     #clean random tweet
         #Charlie has a lot of extra crap in his tweets
         #(copyrights, 'x', twitter links, etc)
@@ -61,36 +117,24 @@ def status(statusList, degree = 1):
         #   a substring of everything up to the symbol will cut out a
         #   good deal. Then check after for his 'signature'.
     #Char cleaning
-    xIndex = tweet.find('\nx')
-    xoxIndex = tweet.find('xox')
-    cIndex = tweet.find('\xa9')
-    #'x' cleaning
-    if(strDel(xIndex, tweet)):
-        strCut(xIndex)
-    #'xox' cleaning
-    elif(strDel(xoxIndex,tweet)):
-        strCut(xoxIndex)
-    # copyright cleaning
-    elif(strDel(cIndex, tweet)):
-        strCut(cIndex)
-    #end of string newline cleaning
-    newlineFlag = False
-    while(not newlineFlag):
-        if(tweet.endswith("\n")):
-            tweet = tweet[:-1]
-        else:
-            newlineFlag = True
+    strDel('\nx')
+    strDel('xox')
+    strDel('\xa9')
+    cleanNewline()
     print "Double newline found at index: " + str(tweet.find('\n\n'))
     print tweet
+
     #determine tweet substring
     def scopetest():
         print "Degree is: " + str(degree)
     scopetest()
+    subTweet()
+    print tweet
 
 api = setupAPI()
 user = getUserToQuery()
 timeline = api.GetUserTimeline(screen_name=user,count=20,include_rts=False)
 print [t.GetText() for t in timeline]
 #print timeline[0].text
-status(timeline)
+status(timeline, 2)
 print "EOT"
