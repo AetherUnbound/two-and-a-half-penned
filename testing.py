0c2c1971259d0f0e04b78ab6248c29a954a9c49a
AetherUnbound <bowdenm@spu.edu>
2015-12-18 22:08:33 -0800

    Minor testing for TTS
    TTS doesn't appear to work on Windows 8. Will try in a linux
    environment.

--- testing.py
+++ testing.py
@@ -1,4 +1,10 @@
 import twitter
+import pyttsx
 
 #help("twitter.Api.GetUserTimeline")
-help("twitter.Status")
+#help("twitter.Status")
+
+engine = pyttsx.init()
+engine.say('Greetings!')
+engine.say('How are you today?')
+engine.runAndWait()
